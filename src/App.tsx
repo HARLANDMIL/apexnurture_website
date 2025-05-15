554c8cd0b18b45dcc8ac896af5c272309e235deb
Milton H <Harlandmilton06@gmail.com>
2025-05-15 16:47:30 +0200

    Upadted FIles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import ProblemSolution from './components/sections/ProblemSolution';
 import HowItWorks from './components/sections/HowItWorks';
 import Testimonials from './components/sections/Testimonials';
 import Services from './components/sections/Services';
-
 import Pricing from './components/sections/Pricing';
 import SuccessStories from './components/sections/SuccessStories';
 import BookACall from './components/sections/BookACall';
@@ -17,11 +16,12 @@ import ParticleBackground from './components/ui/ParticleBackground';
 import TermsOfService from './components/sections/TermsOfService';
 import PrivacyPolicy from './components/sections/PrivacyPolicy';
 import RefundPolicy from './components/sections/RefundPolicy';
+
+// Import the missing AI Suite pages
 import SmartBusinessOps from './sections/SmartBusinessOps';
 import RecruitmentAutomation from './sections/RecruitmentAutomation';
 import LeadToClient from './sections/LeadToClient';
 import PrecisionAIProgram from './sections/PrecisionAIProgram';
-import PrecisionAICard from './sections/PrecisionAICard';
 
 function App() {
   // Handle fade-in on scroll for .fade-in-on-scroll
@@ -63,11 +63,9 @@ function App() {
                 <ProblemSolution />
                 <HowItWorks />
                 <Services />
-                <Pricing />
-                {/* Insert PrecisionAICard below Pricing and above Testimonials as requested */}
-                <PrecisionAICard />
                 <Testimonials />
-                {/* <BriefOverview /> */}
+                <Pricing />
+                <BriefOverview />
                 <SuccessStories />
                 <BookACall />
                 <FAQ />
